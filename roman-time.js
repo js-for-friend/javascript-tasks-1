a2e26f0e76e814e4d142c3152ab883374a23bdc6
skhoborov <skhoborov@yandex.ru>
2016-08-12 16:57:41 +0300

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,5 +1,44 @@
-var hours = process.argv[2];
-var minutes = process.argv[3];
+//var hours = process.argv[2];
+//var minutes = process.argv[3];
+
+
+var hours = prompt('Который час?', 24);
+
+if (num < 0 || num > 23) {
+          alert("Введите значение в диапозоне от 0 до 23");
+          return;
+        }
+
+}
+
+
+var minutes = promt('Введите минуты', 59);
+while (hours > 0) || (hours < 25) {
+ console.log('Время' + minutes); 
+}
+
+
+
+
+
+
+
+
+
 
 // Немного замечательного кода и магии
 // Немного изменений для pull request) 
+//На вход скрипт всегда принимает два числа через пробел:
+
+вначале часы hours (от 0 до 23),
+затем минуты minutes (от 0 до 59).
+Вызов скрипта:
+
+node roman-time.js 15 40
+Должен выводить время римскими цифрами через «:»
+
+XV:XL
+Если время указано неверно:
+
+node roman-time.js 25 40
+Скрипт должен выводить «Время указано не верно».
