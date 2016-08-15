33bd0cecf055093307391b474a0c715dea4fafad
skhoborov <skhoborov@yandex.ru>
2016-08-15 21:31:23 +0300

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,14 +1,14 @@
-var hours = process.argv[2];
+var hours = process.argv[2]; //ввод любых значений чисел
 var minutes = process.argv[3];
-if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59 hours) {
-    console.log('Время указано неверно')
+if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59 ) {
+    console.log('Время указано неверно') //сортировка диапазона чисел
 } else {
-    var time = toRome(hours)+ ':' + toRome(minutes);
-    console.log(time+'\n');
+    var time = toRome(hours)+ ':' + toRome(minutes); //группировка вывода, обращение к фукции
+    console.log(time);	//вывод значения			//для перевода в римские числа
 }
 
 function toRome(numb) {
-    var arabNumb=[1,4,5,9,10,40,50];
+    var arabNumb=[1,4,5,9,10,40,50]; //массив чисел
     var romNumb= ["I", "IV", "V", "IX", "X", "XL", "L"];
     if ((!numb)||(numb == 0)) {
         return '';
