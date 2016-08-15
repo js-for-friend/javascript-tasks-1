779c19ca4dfc3c5b44605f1d0dd9ec296415f682
AndrewBatylov <Andjak007@gmail.com>
2016-08-15 23:52:53 +0300

    js-1

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,12 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
-
-// Немного замечательного кода и магии
+if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59 ) {
+    console.log('Время указано неверно')
+} else {
+    var time = toRome(hours)+ ':' + toRome(minutes);
+    console.log(time);
+}
+function toRome(numb) {
+    var arabNumb=[1,4,5,9,10,40,50];
+    var romNumb= ["I", "IV", "V", "IX", "X", "XL", "L"];
+} return '';
