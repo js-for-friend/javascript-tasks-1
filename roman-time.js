43c8b434a65b2e71eec5f328100389812384ee1b
AndrewBatylov <Andjak007@gmail.com>
2016-08-23 21:23:47 +0300

    Правка

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,21 @@
-var hours = process.argv[2];
-var minutes = process.argv[3];
-if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59 ) {
-    console.log('Время указано неверно')
-} else {
-    var time = toRome(hours)+ ':' + toRome(minutes);
+var hours = Number(process.argv[2]);
+var minutes = Number(process.argv[3]);
+
+// Немного замечательного кода и магии
+function toRim(n) {
+    var ara = [50, 40, 10, 9, 5, 4, 1];
+    var rim = ['L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    for (var i = 0; i < ara.length; i++) {
+        if(n < 1)
+            return "";
+        if(n >= ara[i]) {
+            return rim[i] + toRim(n - ara[i]);
+        }
+    }
+}
+if ( hours < 0 ||hours > 23 ||minutes < 0 ||minutes > 59  )
+    console.log("В сутках 24 часа,исправь значения!");
+else {
+    var time = toRim(hours)+ ':' + toRim(minutes);
     console.log(time);
 }
-function toRome(numb) {
-    var arabNumb=[1,4,5,9,10,40,50];
-    var romNumb= ["I", "IV", "V", "IX", "X", "XL", "L"];
-} return '';
